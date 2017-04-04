5034c971655a6f7ea65e4f8623e8e11341f235c4
Arjan van Eersel <arjan@vaneersel.eu>
2017-04-04 14:23:38 +0300

    Fixed wrong route for admin edit support to User edit handler

--- routes.go
+++ routes.go
@@ -25,6 +25,6 @@ func UserRoutes(r *mux.Router, v *users.UserViews) {
 	//ToDo: Delete
 
 	r.HandleFunc("/admin/user", users.RequireAdmin(v, v.AdminIndexHandler))
-	r.HandleFunc("/account/edit", users.RequireLogin(v, v.UpdateHandler)).Methods("POST")
-	r.HandleFunc("/account/edit/{id}", users.RequireLogin(v, v.EditViewHandler))
+	r.HandleFunc("/admin/user/edit", users.RequireLogin(v, v.UpdateHandler)).Methods("POST")
+	r.HandleFunc("/admin/user/edit/{id}", users.RequireLogin(v, v.EditViewHandler))
 }
